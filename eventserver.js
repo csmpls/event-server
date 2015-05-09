e58c2065ceb3e9f93783cf833a2b132454319da2
elsehow <indra@web482.webfaction.com>
2015-05-09 06:00:30 +0000

    allowed CORS

--- eventserver.js
+++ eventserver.js
@@ -6,6 +6,15 @@ port = 14191
 var server = restify.createServer({});
 server.use(restify.bodyParser())
 
+// allow CORS
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+)
+
 // setup db
 database.syncEventModel()
 
